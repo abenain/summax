08488461f1f485ecd1a6572e9eb14c0d0aa484fe
Antoine Benain <antoine.benain@gmail.com>
2020-06-08 10:14:59 +0200

    Added react-navigation support

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,32 @@
 import * as eva from '@eva-design/eva'
-import { ApplicationProvider, Layout, Text } from '@ui-kitten/components'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { ApplicationProvider } from '@ui-kitten/components'
 import * as Localization from 'expo-localization'
 import i18n from 'i18n-js'
 import React from 'react'
-import { StyleSheet } from 'react-native'
 import en from '../assets/i18n/en'
 import fr from '../assets/i18n/fr'
+import { Home as HomeScreen } from './screens/Home'
 
 i18n.fallbacks = true
 i18n.translations = { fr, en }
 i18n.locale = Localization.locale
 
-const HomeScreen = () => (
-  <Layout style={styles.container}>
-    <Text category='h1'>{i18n.t('Home')}</Text>
-  </Layout>
-)
+export default () => {
+  const Stack = createStackNavigator()
 
-export default () => (
-  <ApplicationProvider {...eva} theme={eva.light}>
-    <HomeScreen/>
-  </ApplicationProvider>
-)
-
-const styles = StyleSheet.create({
-  container: {
-    flex          : 1,
-    justifyContent: 'center',
-    alignItems    : 'center'
-  },
-})
+  return (
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName={'Home'}
+          screenOptions={{
+            headerShown: false
+          }}>
+          <Stack.Screen name='Home' component={HomeScreen}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ApplicationProvider>
+  )
+}
