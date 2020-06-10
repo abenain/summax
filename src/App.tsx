0df30e63e0736dd7cdf2a0f9b64cb2ec55a87160
Antoine Benain <antoine.benain@gmail.com>
2020-06-10 17:14:25 +0200

    Handle double splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,10 @@ import { ApplicationProvider, IconRegistry } from '@ui-kitten/components'
 import { EvaIconsPack } from '@ui-kitten/eva-icons'
 import Constants from 'expo-constants'
 import * as Localization from 'expo-localization'
+import * as SplashScreen from 'expo-splash-screen'
 import i18n from 'i18n-js'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Image, View } from 'react-native'
 import { Provider } from 'react-redux'
 import en from '../assets/i18n/en'
 import fr from '../assets/i18n/fr'
@@ -15,6 +17,9 @@ import { getStore } from './redux/store'
 import { Home as HomeScreen } from './screens/home'
 import { Login as LoginScreen } from './screens/login'
 import { SignUp as SignUpScreen } from './screens/signup'
+
+const MIN_SPLASH_SCREEN_DURATION_MS = 2000
+const splashWithPeople = require('../assets/splash_with_people.png')
 
 i18n.fallbacks = true
 i18n.translations = { fr, en }
@@ -29,6 +34,38 @@ export type RootStackParamList = {
 export default () => {
   const Stack = createStackNavigator()
   const store = getStore()
+  const [showSplashScreen, setShowSplashScreen] = useState(true)
+  const [appIsReady, setAppIsReady] = useState(false)
+
+  useEffect(function componentDidMount(){
+    SplashScreen.preventAutoHideAsync()
+  }, [])
+
+  function doLoadAssets(){
+    return new Promise((resolve) => {
+      setTimeout(resolve, 1000)
+    })
+  }
+
+  async function startLoadingAssets(){
+    SplashScreen.hideAsync()
+
+    setTimeout(() => setShowSplashScreen(false), MIN_SPLASH_SCREEN_DURATION_MS)
+
+    await doLoadAssets()
+    setAppIsReady(true)
+  }
+
+  if(!appIsReady || showSplashScreen){
+    return (
+      <View style={{flex: 1}}>
+        <Image
+          style={{height: '100%', width: '100%'}}
+          source={splashWithPeople}
+          onLoad={startLoadingAssets} />
+      </View>
+    )
+  }
 
   return (
     <Provider store={store}>
