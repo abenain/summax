f23c2300a580513ac7fab7b0dda1ff80c2cd3234
Antoine Benain <antoine.benain@gmail.com>
2020-06-18 08:32:54 +0200

    Added expo video player to training screen

--- src/screens/training/index.tsx
+++ src/screens/training/index.tsx
@@ -1,22 +1,36 @@
-import { Layout, Text } from '@ui-kitten/components'
-import { useEffect, useState } from 'react'
+import { Layout } from '@ui-kitten/components'
+import { Video } from 'expo-av'
+import VideoPlayer from 'expo-video-player'
 import * as React from 'react'
+import { useEffect, useState } from 'react'
+import { Dimensions } from 'react-native'
 import { Loading } from '../../components/Loading'
 
-export function TrainingScreen(){
+export function TrainingScreen() {
   const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     setTimeout(() => setIsLoading(false), 2000)
   }, [])
 
-  if(isLoading){
-    return <Loading />
+  if (isLoading) {
+    return <Loading/>
   }
 
   return (
-    <Layout style={{alignItems: 'center', flex: 1, justifyContent: 'center'}}>
-      <Text category={'h1'}>Training</Text>
+    <Layout style={{ alignItems: 'center', flex: 1 }}>
+      <VideoPlayer
+        videoProps={{
+          shouldPlay: true,
+          resizeMode: Video.RESIZE_MODE_CONTAIN,
+          source    : {
+            uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+          },
+        }}
+        inFullscreen={false}
+        height={233}
+        width={Dimensions.get('window').width}
+      />
     </Layout>
   )
 }
