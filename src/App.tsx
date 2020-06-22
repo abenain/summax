2e9eead38cdcdc65997ff9cb18726fa3d6b07350
Antoine Benain <antoine.benain@gmail.com>
2020-06-22 16:49:43 +0200

    Refactored Stack Navigator to a standalone component

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,23 @@
 import * as eva from '@eva-design/eva'
 import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator, StackHeaderTitleProps } from '@react-navigation/stack'
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components'
 import { EvaIconsPack } from '@ui-kitten/eva-icons'
-import Constants from 'expo-constants'
 import * as Font from 'expo-font'
 import * as Localization from 'expo-localization'
 import * as SplashScreen from 'expo-splash-screen'
 import i18n from 'i18n-js'
 import React, { useEffect, useState } from 'react'
-import { Image, Platform, View } from 'react-native'
+import { Image, View } from 'react-native'
 import { Provider } from 'react-redux'
 import en from '../assets/i18n/en'
 import fr from '../assets/i18n/fr'
-import { HeaderTitle } from './components/header-title'
+import { MainStackNavigator } from './navigation/MainStackNavigator'
 import { ActionType } from './redux/actions'
 import { getStore } from './redux/store'
-import { HomeScreen } from './screens/home'
-import { LoginScreen } from './screens/login'
-import { RewardScreen } from './screens/reward'
-import { SignUpScreen } from './screens/signup'
-import { TrainingScreen } from './screens/training'
-import { WorkoutScreen } from './screens/workout'
 import * as Homepage from './webservices/homepage'
 
 const MIN_SPLASH_SCREEN_DURATION_MS = 2000
 const splashWithPeople = require('../assets/splash_with_people.png')
-const arrowLeftIcon = require('../assets/arrow-left.png')
 
 i18n.fallbacks = true
 i18n.translations = { fr, en }
@@ -45,7 +36,6 @@ export type RootStackParamList = {
 }
 
 export default () => {
-  const Stack = createStackNavigator()
   const store = getStore()
   const [showSplashScreen, setShowSplashScreen] = useState(true)
   const [assetsLoaded, setAssetsLoaded] = useState(false)
@@ -100,31 +90,7 @@ export default () => {
 
       <ApplicationProvider {...eva} theme={eva.light}>
         <NavigationContainer>
-          <Stack.Navigator
-            initialRouteName={'Home'}
-            screenOptions={{
-              headerBackImage       : () => <Image source={arrowLeftIcon}
-                                                   style={{ height: 24, marginLeft: 16, width: 24 }}/>,
-              headerBackTitleVisible: false,
-              headerStyle           : {
-                height: (Platform.OS === 'ios' ? Constants.statusBarHeight : 0) + 56,
-              },
-              headerTitle           : HeaderTitle,
-            }}>
-            <Stack.Screen name='Login' component={LoginScreen} options={{ headerShown: false }}/>
-            <Stack.Screen name='SignUp' component={SignUpScreen} options={{ headerShown: false }}/>
-            <Stack.Screen name='Home' component={HomeScreen}/>
-            <Stack.Screen name='Reward' component={RewardScreen} options={{ headerShown: false }}/>
-            <Stack.Screen name='Workout'
-                          component={WorkoutScreen}
-                          options={({ route }) => ({
-                            headerTitle      : (props: StackHeaderTitleProps) => <HeaderTitle
-                              title={route.params['title']} {...props}/>,
-                            headerTransparent: true,
-                          })}
-            />
-            <Stack.Screen name='Training' component={TrainingScreen} options={{ headerShown: false }}/>
-          </Stack.Navigator>
+          <MainStackNavigator />
         </NavigationContainer>
       </ApplicationProvider>
     </Provider>
---
+++ src/navigation/MainStackNavigator.tsx
@@ -0,0 +1,45 @@
+import { createStackNavigator, StackHeaderTitleProps } from '@react-navigation/stack'
+import Constants from 'expo-constants'
+import * as React from 'react'
+import { Image, Platform } from 'react-native'
+import { HeaderTitle } from '../components/header-title'
+import { HomeScreen } from '../screens/home'
+import { LoginScreen } from '../screens/login'
+import { RewardScreen } from '../screens/reward'
+import { SignUpScreen } from '../screens/signup'
+import { TrainingScreen } from '../screens/training'
+import { WorkoutScreen } from '../screens/workout'
+
+const arrowLeftIcon = require('../../assets/arrow-left.png')
+
+export function MainStackNavigator() {
+  const Stack = createStackNavigator()
+
+  return (
+    <Stack.Navigator
+      initialRouteName={'Home'}
+      screenOptions={{
+        headerBackImage       : () => <Image source={arrowLeftIcon}
+                                             style={{ height: 24, marginLeft: 16, width: 24 }}/>,
+        headerBackTitleVisible: false,
+        headerStyle           : {
+          height: (Platform.OS === 'ios' ? Constants.statusBarHeight : 0) + 56,
+        },
+        headerTitle           : HeaderTitle,
+      }}>
+      <Stack.Screen name='Login' component={LoginScreen} options={{ headerShown: false }}/>
+      <Stack.Screen name='SignUp' component={SignUpScreen} options={{ headerShown: false }}/>
+      <Stack.Screen name='Home' component={HomeScreen}/>
+      <Stack.Screen name='Reward' component={RewardScreen} options={{ headerShown: false }}/>
+      <Stack.Screen name='Workout'
+                    component={WorkoutScreen}
+                    options={({ route }) => ({
+                      headerTitle      : (props: StackHeaderTitleProps) => <HeaderTitle
+                        title={route.params['title']} {...props}/>,
+                      headerTransparent: true,
+                    })}
+      />
+      <Stack.Screen name='Training' component={TrainingScreen} options={{ headerShown: false }}/>
+    </Stack.Navigator>
+  )
+}
