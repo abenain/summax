208a09b1701ddcd45209219d4a2e755017c15f23
Antoine Benain <antoine.benain@gmail.com>
2021-01-21 21:37:17 +0100

    v2.4.1 fixed premium content access

--- src/hooks/useNavigateToWorkout.ts
+++ src/hooks/useNavigateToWorkout.ts
@@ -3,17 +3,18 @@ import { useCallback } from 'react'
 import { useSelector } from 'react-redux'
 import { GlobalState } from '../redux/store'
 import { Workout, WorkoutPlan } from '../types'
+import { isPremium } from '../utils'
 
 export function useNavigateToWorkout() {
   const navigation = useNavigation()
   const { subscriptionPeriodEnd } = useSelector(({ userData: { user } }: GlobalState) => user.valueOr({} as any))
 
-  const isPremium = useCallback(() => {
+  const userIsPremium = useCallback(() => {
     return isPremium(subscriptionPeriodEnd)
   }, [subscriptionPeriodEnd])
 
   return function(workout: Workout){
-    if(workout.plan === WorkoutPlan.PREMIUM && isPremium() === false){
+    if(workout.plan === WorkoutPlan.PREMIUM && userIsPremium() === false){
       return navigation.navigate('Subscription')
     }
 
